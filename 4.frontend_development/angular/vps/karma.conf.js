1a70af96484578713140841a0005b47757995fa2
vps4618 <vishwapraveen4618@gmail.com>
2022-08-12 17:23:25 +0530

    strict mode

--- 4.frontend_development/angular/vps/karma.conf.js
+++ 4.frontend_development/angular/vps/karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/my_first_project'),
+      dir: require('path').join(__dirname, './coverage/vps'),
       subdir: '.',
       reporters: [
         { type: 'html' },
