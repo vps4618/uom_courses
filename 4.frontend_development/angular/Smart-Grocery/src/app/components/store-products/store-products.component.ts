e10e42c5c62830db317ce2e31e780907a33c91ce
vps4618 <vishwapraveen4618@gmail.com>
2022-08-14 13:08:37 +0530

    ng Directives  added to smart-grocery app

--- 4.frontend_development/angular/Smart-Grocery/src/app/components/store-products/store-products.component.ts
+++ 4.frontend_development/angular/Smart-Grocery/src/app/components/store-products/store-products.component.ts
@@ -33,4 +33,50 @@ export class StoreProductsComponent implements OnInit {
   public firstProductItemName = "White Basmathi Rice";
   public quantityOfRice = 75;
 
+  //attributte directives
+  public isLowInventory = false;
+  public dhalStorage = 10;
+
+  public getDhalStorage(){
+     if(this.dhalStorage<50){
+       this.isLowInventory = true;
+     }
+
+     return this.dhalStorage;
+  }
+
+  //structural directives
+  public products = [{
+    "productId":"001",
+    "productName":"White Basmathi Rice",
+    "createdDate":"Jan 29, 2020",
+    "unitPrice":"Rs. 400",
+    "quantity":100,
+    "productDescription":"White Basmathi Rice imported from pakistan"
+  },
+  {
+    "productId":"002",
+    "productName":"Flour",
+    "createdDate":"Jan 29, 2020",
+    "unitPrice":"Rs. 190",
+    "quantity":50,
+    "productDescription":"Super Fine Whole grain general Purpose flour"
+  },
+  {
+    "productId":"003",
+    "productName":"Sugar",
+    "createdDate":"Jan 29, 2020",
+    "unitPrice":"Rs. 200",
+    "quantity":1200,
+    "productDescription":"White sugar manufactured by Palwatte Factory"
+  },
+  {
+    "productId":"004",
+    "productName":"Dhal",
+    "createdDate":"Jan 29, 2020",
+    "unitPrice":"Rs. 200",
+    "quantity":10,
+    "productDescription":"Imported mysoor dhal from India"
+ }]
+
 }
