de749b72aa99fda98a6eeeae9c735aaf62334f0a
vps4618 <vishwapraveen4618@gmail.com>
2022-08-22 13:17:36 +0530

    api

--- 5.server-side-web-programming/js-mastery-api/index.js
+++ 5.server-side-web-programming/js-mastery-api/index.js
@@ -1,1 +1,19 @@
 import express from "express";
+import bodyParser from "body-parser";
+import usersRoutes from "./routes/users.js";
+const app = express();
+const PORT  = 5000;
+
+//this gonna say we use json data in our whole application
+app.use(bodyParser.json());
+
+app.use('/users',usersRoutes);
+
+app.get('/',(req,res) => {
+    console.log('[TEST]!');
+    res.send('Hello from home page');
+})
+
+app.listen(PORT, () => {
+    console.log(`Server running on port : http://localhost:${PORT}`)
+});
