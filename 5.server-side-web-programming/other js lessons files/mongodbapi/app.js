9d9da04d53fce886d37129713a88e332c5b3d96f
vishwa praveen <vishwapraveen4618@gmail.com>
2022-08-28 23:22:13 +0530

    new mongodb api

--- 5.server-side-web-programming/other js lessons files/mongodbapi/app.js
+++ 5.server-side-web-programming/other js lessons files/mongodbapi/app.js
@@ -1,7 +1,12 @@
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
+const bodyParser= require('body-parser'); 
+const cors = require('cors');
 require('dotenv/config');
+
+app.use(bodyParser.json());
+app.use(cors());
 
 const PORT = process.env.PORT || 3000;
 
--- 5.server-side-web-programming/other js lessons files/mongodbapi/routes/posts.js
+++ 5.server-side-web-programming/other js lessons files/mongodbapi/routes/posts.js
@@ -3,12 +3,70 @@ const res = require('express/lib/response');
 const router = express.Router();
 const Post  = require('../models/Post');
 
-router.get('/',(req,res) => {
-    res.send('We are on posts');
+//getting all posts
+router.get('/',async (req,res) => {
+    // getting all posts
+    try{
+        const posts = await Post.find();
+        res.json(posts);
+    }
+    catch(err){
+        res.json({message:err});
+    }
 });
 
-router.post('/',(req,res) => {
+// submit  a post
+router.post('/', async (req,res) => {
+    
+    const post = new Post({
+        title:req.body.title,
+        description:req.body.description
+    });
+    
+    // saving a post to database
+    try{
+        
+        const savedPost = await post.save();
+        res.json(savedPost);
 
+    }
+    catch(err){
+        res.json({message:err});
+    }
+
+});
+
+// get a specific post
+router.get('/:postId',async (req,res) => {
+    try{
+        const post = await Post.findById(req.params.postId);
+        res.json(post);
+    }
+    catch(err){
+        res.json({message:err});
+    }    
+});
+
+// delete a specific post
+router.delete('/:postId',async (req,res) => {
+    try{
+        const removedPost = await Post.remove({_id:req.params.postId});
+        res.json(removedPost);
+    }
+    catch(err){
+        res.json({message:err});
+    }
+});
+
+// update a post
+router.patch('/:postId',async (req,res) => {
+    try{
+        const updatedPost = await Post.updateOne({_id:req.params.postId},{$set:req.body});
+        res.json(updatedPost);
+    }
+    catch(err){
+        res.json(err);
+    }
 });
 
 //middle wares (executes when we hit an route)
