f1e36c6935c5c9a4b82d3ff0c8a5eddf2bd35400
vishwa praveen <vishwapraveen4618@gmail.com>
2022-09-24 10:54:42 +0530

    other-things-to-know-about-sets learned

---
+++ 2.python programming/1.2 Sets/things-to-know.py
@@ -0,0 +1,15 @@
+# initialise a with {}
+a = {} # ! this creates empty dictionary not a set
+
+# check data type of a
+print(type(a))
+
+# ! initialise a with set()
+a = set()
+
+# check data type of a
+print(type(a))
+
+# ! set cannot have mutable items
+# this will cause an error.
+# my_set = {1, 2, [3, 4]}
